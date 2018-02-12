109a8d63a1ab0355513c36f92a226bcaa995f502
Michael Rooney <m.rooney93@gmail.com>
2018-02-12 15:23:27 +0000

    Add tests for task 3

---
+++ Assignment 5/Task 3/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -a GREP_ARGS=("KEYWORD" "KEYWORD -n" "KEYWORD -v" "KEYWORD -c")
+# Note: need trailing space on each result
+declare -a GREP_RESULTS=("aaaKEYWORDbbb " "\x1B[32m1:\x1B[0maaaKEYWORDbbb " "cccdddeeefff " "aaa\x1B[31mKEYWORD\x1B[0mbbb ")
+
+for value in {0..3}
+do
+	grep_result=$(cat test.txt | ./my_grep ${GREP_ARGS[value]})
+	# Output includes newlines so we replace them with spaces
+	grep_result=$(echo $grep_result | tr '\n' ' ')
+	# Need this step to properly compare colours
+	expected_result=$(echo -e "${GREP_RESULTS[value]}")
+	if [ "$grep_result" == "$expected_result" ]; then
+		echo "Test passed: $grep_result"
+	else
+		echo "Test failed: expected $expected_result but got $grep_result"
+	fi
+done
