04f362d3c34c34d1d30c2c7f7a20cd52ef803844
m <m.rooney93@gmail.com>
2018-02-12 12:43:51 -0800

    Add report, cleanup

--- Assignment 5/Task 3/test.sh
+++ Assignment 5/Task 3/test.sh
@@ -2,7 +2,11 @@
 
 declare -a GREP_ARGS=("KEYWORD" "KEYWORD -n" "KEYWORD -v" "KEYWORD -c" "" "-n -v")
 # Note: need trailing space on each result
-declare -a GREP_RESULTS=("aaaKEYWORDbbb " "\x1B[32m1:\x1B[0maaaKEYWORDbbb " "cccdddeeefff " "aaa\x1B[31mKEYWORD\x1B[0mbbb " "ERROR: please provide a word to search for " "ERROR: please provide a word to search for ")
+declare -a GREP_RESULTS=(
+	"aaaKEYWORDbbbaaKEYWORDcc " "\x1B[32m1:\x1B[0maaaKEYWORDbbbaaKEYWORDcc " "cccdddeeefff " 
+	"aaa\x1B[31mKEYWORD\x1B[0mbbbaa\x1B[31mKEYWORD\x1B[0mcc " 
+	"ERROR: please provide a word to search for " "ERROR: please provide a word to search for "
+)
 
 for value in {0..5}
 do
