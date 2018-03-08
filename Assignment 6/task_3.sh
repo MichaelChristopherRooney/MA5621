f3002987c450f6356cb59976e51932fdc0219644
m <m.rooney93@gmail.com>
2018-03-08 08:22:40 -0800

    Mostly finished

--- Assignment 6/task_3.sh
+++ Assignment 6/task_3.sh
@@ -1,11 +1,29 @@
-declare -a drawn=(0 0 0 0 0 0) # stores numbers drawn so far
-num_drawn=0 # how many draws have been made already
+# Stores numbers drawn so far
+declare -a drawn=(0 0 0 0 0 0)
+
+# How many draws have been made already
+num_drawn=0 
 
 lowest=1
 highest=45
 
+# Generate a random number then check if the above array already contains it.
+# If so then continue to generate random numbers until an unused one is found
 for i in {0..5}
 do
-	ran=$(( (RANDOM % ($highest + 1 - $lowest) + $lowest) ))
+	number_unused=0
+	while [ $number_unused -ne 1 ]; do
+		number_unused=1
+		ran=$(( (RANDOM % ($highest + 1 - $lowest) + $lowest) ))
+		n=0
+		while [ $n -lt $num_drawn ]; do
+			if [ ${drawn[$n]} -eq $ran ]; then
+				number_unused=0
+			fi
+			n=$(( n + 1 ))
+		done
+	done
+	drawn[i]=$ran
+	num_drawn=$(( num_drawn + 1 ))
 	echo "$ran"
 done
